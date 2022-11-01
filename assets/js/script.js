b34ab877fb6cd6e550cf1f19468633dd5160e2b3
omer <mdomerkhan800@gmail.com>
2022-11-01 18:17:44 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-fetch("https://api-mainnet.magiceden.io/auctions/collections/buffbunnybaby?q=%7B%22%24match%22%3A%7B%7D%2C%22%24sort%22%3A%7B%22config.endDate%22%3A1%7D%2C%22%24skip%22%3A0%2C%22%24limit%22%3A20%7D").then(
+fetch("https://api-mainnet.magiceden.io/auctions/collections/buffbunnybaby?q=%7B%22%24match%22%3A%7B%7D%2C%22%24sort%22%3A%7B%22config.endDate%22%3A1%7D%2C%22%24skip%22%3A0%2C%22%24limit%22%3A20%7D")
+.then(
     (response)=> { return response.json; })
     .then((data)=> {console.log(data);
     })
